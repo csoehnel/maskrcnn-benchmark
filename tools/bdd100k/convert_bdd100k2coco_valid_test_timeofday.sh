b94acb26fe9cb411ddcb2337ed2451eb54a3a49e
Christoph Soehnel <csoehnel@gmail.com>
2019-03-21 21:56:27 +0000

    coco conversion for augmented datasets

--- tools/bdd100k/convert_bdd100k2coco_valid_test_timeofday.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python convert_bdd100k2coco.py --bdd100kjson ../../../../../CurrentDatasets/bdd100k_sorted_coco/annotations/bdd100k_sorted_valid.json --cocojson ../../../../../CurrentDatasets/bdd100k_sorted_coco/annotations/bdd100k_sorted_valid_day_coco.json --timeofday daytime &
-python convert_bdd100k2coco.py --bdd100kjson ../../../../../CurrentDatasets/bdd100k_sorted_coco/annotations/bdd100k_sorted_valid.json --cocojson ../../../../../CurrentDatasets/bdd100k_sorted_coco/annotations/bdd100k_sorted_valid_night_coco.json --timeofday night &
-python convert_bdd100k2coco.py --bdd100kjson ../../../../../CurrentDatasets/bdd100k_sorted_coco/annotations/bdd100k_sorted_test.json --cocojson ../../../../../CurrentDatasets/bdd100k_sorted_coco/annotations/bdd100k_sorted_test_day_coco.json --timeofday daytime &
-python convert_bdd100k2coco.py --bdd100kjson ../../../../../CurrentDatasets/bdd100k_sorted_coco/annotations/bdd100k_sorted_test.json --cocojson ../../../../../CurrentDatasets/bdd100k_sorted_coco/annotations/bdd100k_sorted_test_night_coco.json --timeofday night &
