70b268aec6432f21698a2f62de23f1ea33664e81
Christoph Soehnel <csoehnel@gmail.com>
2019-03-27 21:06:45 +0100

    test runner scripts for several object categories

---
+++ tools/bdd100k/test_bdd100k_class_car_bus_truck.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ "$1" == '' ]; then
+    echo "Usage: $0 <model folder>";
+    exit;
+fi
+
+for file in "$1"/*.pth; do
+    python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_valid',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_valid_day',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_valid_night',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_train_dev_A_over',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_train_dev_B_over',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_train_dev_C_over',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_train_A_over_2000',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_train_B_over_2000',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_train_C_over_2000',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+
+    python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_test',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_test_day',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_test_night',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_v032_e14_train_A_ganaug_025_over_2000',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_v032_e14_train_B_ganaug_025_over_2000',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_v032_e14_train_A_ganaug_050_over_2000',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_v032_e14_train_dev_A_ganaug_025_over',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_v032_e14_train_dev_B_ganaug_025_over',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_v032_e14_train_dev_A_ganaug_050_over',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_train_dev_A_ganaug_050_over_baseonly',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+    #python tools/test_net.py --config-file "configs/retinanet/retinanet_R-50-FPN_1x_finetune_nightdrive.yaml" MODEL.WEIGHT "$file" DATASETS.TEST "('bdd100k_coco_train_dev_A_ganaug_050_over_augonlyasbase',)" TEST.CAT_ID_FILTER "[3, 6, 8]"
+done
